e5b8c37dd672996b9800fb272aca9573198f7999
PankajLal99 <lalpankaj0@gmail.com>
2020-04-25 10:30:53 +0530

    db updated

--- db/admin.py
+++ db/admin.py
@@ -2,16 +2,11 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 class BuyerAdmin(admin.ModelAdmin):
-    list_display=['invoice','seller','buyer']
+    list_display=['invoice','seller','buyer','status']
     list_filter=['invoice_date']
     search_fields=['invoice']
 
     
-    
 
-
-admin.site.register(models.Buyer)
 admin.site.register(models.Billing,BuyerAdmin)
 admin.site.register(models.Products)
-admin.site.register(models.Status)
-admin.site.register(models.Seller)
--- db/models.py
+++ db/models.py
@@ -1,36 +1,34 @@
 from django.db import models
 
 # Create your models here.
-
-class Buyer(models.Model):
-    buyer_code=models.CharField(max_length=20,primary_key=True)
-    buyer_name=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.buyer_name
-
-class Seller(models.Model):
-    seller_name=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.seller_name
+buylist=[
+    ("JILA","JILA MD UNION TEST ADDREESS \n GST NO : !12343557 \n TESTING"),
+]
+selllist=[
+    ("PARESH","PARESHTRADERS SELLER SELECTED \n BHOPAL \n \n (*@^)%!#@@ ")
+]
+stat= [
+    (1,"PAID"),
+    (0,"UNPAID")
+]
 
 class Billing(models.Model):
     invoice=models.IntegerField(primary_key=True)
-    buyer=models.ForeignKey('Buyer',on_delete=models.CASCADE)
-    seller=models.ForeignKey('Seller',on_delete=models.CASCADE)
+    buyer=models.CharField(max_length=300,choices=buylist)
+    seller=models.CharField(max_length=300,choices=selllist)
     challan_no=models.IntegerField()
     buyer_order_no=models.IntegerField()
     invoice_date=models.DateField()
     delivery_date=models.DateField()
     order_date=models.DateField()
     notes=models.TextField()
+    status=models.BooleanField(default=0,choices=stat)
 
     def __str__(self):
         return str(self.invoice)
 
 class Products(models.Model):
-    invoice=models.ForeignKey('Billing',on_delete=models.CASCADE)
+    invoice=models.ForeignKey(Billing,on_delete=models.CASCADE)
     product=models.CharField(max_length=50)
     quantity=models.FloatField()
     rate=models.FloatField()
@@ -44,13 +42,7 @@ class Products(models.Model):
     def __str__(self):
         return str(self.total)
 
-class Status(models.Model):
-    invoice=models.ForeignKey('Billing',on_delete=models.CASCADE)
-    buyer_name=models.ForeignKey('Buyer',on_delete=models.CASCADE)
-    status=models.BooleanField()
-
-    def __str__(self):
-        return str(self.invoice)
+    
     
 
 
